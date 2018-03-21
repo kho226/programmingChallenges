b371ee7027d774a14d3d7e5757537a383b6afa1c
Kyle Ong <kho226@nyu.edu>
2018-03-21 13:43:50 -0400

    avl_tree class

--- de_shaw/avl_tree/avl_tree.py
+++ de_shaw/avl_tree/avl_tree.py
@@ -4,6 +4,14 @@
 
     avl tree
 
+    insert: log(n)
+    left_rotate: O(1)
+    right_rotate: O(1)
+    get_height: O(1)
+    get_balance: O(1)
+    pre_order: (logn)
+
+
 '''
 
 class TreeNode(object):
@@ -38,56 +46,145 @@ class AVLTree(object):
         - [ ] right_rotate
         - [ ] get_height
         - [ ] get_balance
-        - [ ] pre_order
         - [ ] get_balance
         - [ ] pre_order
 
 
     '''
-
-    def __init__(object,val):
-        self.root = TreeNode(val)
-
     
     def insert(root,val):
-    '''
-        Author: Kyle Ong
-        Date: 03/20/2018
+        '''
+            Author: Kyle Ong
+            Date: 03/20/2018
 
-        Type: root: TreeNode
-        Type: val: int
+            Type: root: TreeNode
+            Type: val: int
 
-    '''
-    if not root: return TreeNode(val)
-    if key < root.val: root.left = insert(root.left)
-    else: root.right = insert(root.right)
+        '''
+        if not root: return TreeNode(val)
+        if key < root.val: root.left = self.insert(root.left)
+        else: root.right = self.insert(root.right)
 
-    root.height = 1 + max(get_height(root.left),get_height(root.right))
+        root.height = 1 + max(self.get_height(root.left),self.get_height(root.right))
 
-    load = get_load(root.left,root.right)
+        load = self.get_load(root.left,root.right)
 
-    if load > 1 and val > root.left.val: #left - right case
-        root.left = left_rotate(root.left)
-        return right_rotate(root)
-    if load > 1 and val < root.left.val: #left - left case
-        return right_rotate(root)
-    if load < -1 and val > root.right.val: #right - right case
-        return left_rotate(root)
-    if load < -1 and val < root.right.val: #right - left case
-        root.right = right_rotate(root.right)
-        return left_rotate(root)
+        if load > 1 and val > root.left.val: #left - right case
+            root.left = self.left_rotate(root.left)
+            return self.right_rotate(root)
+        if load > 1 and val < root.left.val: #left - left case
+            return self.right_rotate(root)
+        if load < -1 and val > root.right.val: #right - right case
+            return self.left_rotate(root)
+        if load < -1 and val < root.right.val: #right - left case
+            root.right = self.right_rotate(root.right)
+            return self.left_rotate(root)
     
     return root
 
-    def left_rotate(self,z):
+    def left_rotate(self,x):
+        '''
+            Author: Kyle Ong
+            Date: 03/21/2018
+
+            left rotate for avl tree
+
+            runtime: O(1)
+            space: O(1)
+
+            Type: x: TreeNode
+            Type: Return TreeNode
+        '''
+        y = x.right
+        sub_tree = y.left
+
+        x.left = sub_tree
+        y.left = x
+
+        x.height = 1 + max(self.get_height(x.left),self.get_height(x.right))
+        y.height = 1 + max(self.get_height(y.left), self.get_height(y.right))
+
+        return y
+
+
+    def right_rotate(self,x):
+        '''
+            Author: Kyle Ong
+            Date: 03/21/2018
+
+            right rotate for avl tree
+
+            runtime: O(1)
+            space: O(1)
+
+            Type: x: TreeNode
+            Type: Return: TreeNode
+
+        '''
+        y = x.left
+        sub_tree = y.left
+
+        y.right = x
+        x.right = sub_tree
+
+        y.height = 1 + max(self.get_height(y.left),self.get_height(y.right))
+        x.height = 1 + max(self.get_height(x.left), self.get_height(x.right))
+
+        return y
+
+
+    def get_height(self,root):
+        '''
+            Author: Kyle Ong
+            Date: 03/21/2018
+
+            get_height for avl tree
+
+            Type: root: TreeNode
+            Type: Return: int
+
+            runtime:O(logn)
+            space:O(1)
+
+        '''
+        if not root: return 0
+        return root.height
     
-    def right_rotate(self,z):
+    def get_balance(self,root):
+        '''
+            Author: Kyle Ong
+            Date: 03/21/2018
+
+            get_balance for avl tree
+
+            Type: root: TreeNode
+            Type: balance: int
+
+            runtime: O(logn)
+            space: O(1)
+        '''
+        if not root: return 0
+        balance = self.get_height(x.left) - self.get_height(x.right)
+        return balance
+        
     
-    def get_height(self,z):
+    def pre_order(self,root):
+        '''
+            Author: Kyle Ong
+            Date: 03/21/2018
+
+            pre_order for avl tree
+
+            runtime: O(logn)
+            space: O(1)
+
+            Type: root: TreeNode
+            Type: Return: None
+        '''
     
-    def get_balance(self,z):
-    
-    def pre_order(self,z):
-    
+        if not root: return
+        print("{0} ".format(root.val))
+        self.pre_order(root.left)
+        self.pre_order(root.right)
 
     
