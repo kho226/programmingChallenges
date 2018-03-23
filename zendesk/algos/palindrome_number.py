ef2723b4514e27ada43a079de15cd34765033602
Kyle Ong <kho226@nyu.edu>
2018-03-23 11:40:11 -0400

    added some more

--- zendesk/algos/palindrome_number.py
+++ zendesk/algos/palindrome_number.py
@@ -1,4 +1,36 @@
 '''
 
-    just reverse and test for equality
+    Author: Kyle Ong
+    Date: 03/22/2018
+
+    palindrome_number
 '''
+
+def palindrome_number(x):
+    ''' 
+        Author: Kyle Ong
+        Date: 03/23/2018
+
+        type: x: int
+        rtype: ret: Bool
+
+        runtime: O(n)
+        space: O(n)
+
+    '''
+    ret = False
+    if not x: return ret
+    
+    tmp = x
+    p = 0
+
+    while tmp > 0:
+        p *= 10
+        p += tmp % 10
+        tmp = int(tmp/10)
+    
+    if p == x
+        ret = True
+    return ret
+    
+    
--- zendesk/algos/reverse_integer.py
+++ zendesk/algos/reverse_integer.py
@@ -1,4 +1,35 @@
 '''
 
-    remember to test the reversed integer for overflow with int.bit_length()
+    Author: Kyle Ong
+    Date: 03/23/2018
+
+    runtime: O(n)
+    space: O(n)
 '''
+
+def reverse_integer(x):
+    '''
+        Author: Kyle Ong
+        Date: 03/23/2018
+
+        type: x: int
+
+        rtype: int
+
+        will return 0 if reversed integer overflows signed 32 bit
+
+    '''
+    ret = 0
+    if not x: return ret
+
+    tmp = x*-1 if x < 0 else x
+
+    while tmp > 0:
+        ret *= 10
+        ret += tmp % 10
+        tmp = int(tmp/10)
+    
+    ret = ret*-1 if x < 0 else ret
+    if ret.bit_length() >= 32: ret = 0
+    return ret
+
